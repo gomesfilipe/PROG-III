c6dd1123a36a18039eaccbd93264e27ea7283bd7
gomesfilipe <filipejvmf@gmail.com>
2021-11-19 17:44:09 -0300

    Exercício 1

--- lista-exercicios-3/J2_01.java
+++ lista-exercicios-3/J2_01.java
@@ -2,27 +2,35 @@ import java.util.Random;
 import java.util.Scanner;
 
 class Aleatorio {
-	private Random random;
+	private static Random random;
 	private int sorteado;
 	private int maximo;
 	
 	public static final int VALOR_MAXIMO_DEFAULT = 100;
 	
+	static {
+		Scanner s = new Scanner(System.in);
+		random = new Random(s.nextLong());
+		s.close();
+	}
+	
 	public Aleatorio(int maximo) {
 		this.maximo = maximo;
-		
+		this.sorteado = random.nextInt(maximo);
 	}
 	
 	public Aleatorio() {
+		
 		this.maximo = VALOR_MAXIMO_DEFAULT;
+		this.sorteado = random.nextInt(maximo);
 	}
 
-	public Random getRandom() {
+	public static Random getRandom() {
 		return random;
 	}
 
-	public void setRandom(Random random) {
-		this.random = random;
+	public static void setRandom(Random random) {
+		Aleatorio.random = random;
 	}
 
 	public int getSorteado() {
@@ -44,28 +52,16 @@ class Aleatorio {
 	public static int getValorMaximoDefault() {
 		return VALOR_MAXIMO_DEFAULT;
 	}
-	
-	public int sorteiaNumero() {
-		int sorteado = this.random.nextInt(this.maximo);
-		this.setSorteado(sorteado);
-		return sorteado;
-	}
 }
 
 class J2_01 {
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		Aleatorio aleatorio = new Aleatorio();
-		Random gerador = new Random(s.nextLong());
-		aleatorio.setRandom(gerador);
 		
 		for(int i = 0; i < 10; i++) {
-			System.out.print(aleatorio.sorteiaNumero());
-			System.out.print(" ");
+			Aleatorio aleatorio = new Aleatorio();
+			System.out.print(aleatorio.getSorteado() + " ");
 		}
 		
 		System.out.println();
-		
-		s.close();
 	}
 }
