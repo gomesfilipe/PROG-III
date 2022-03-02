aa699e9164a5c647997fd7fc593800a04444fe07
gomesfilipe <filipejvmf@gmail.com>
2022-03-02 16:29:05 -0300

    Biblioteca do professor vitor adicionada

--- trabalho-sistema-eleitoral-brasileiro-cpp/include/Person.h
+++ trabalho-sistema-eleitoral-brasileiro-cpp/include/Person.h
@@ -1,11 +1,22 @@
 #ifndef PERSON_H_
 #define PERSON_H_
 
+#include <ctime>
 #include <string>
 #include <iostream>
 using namespace std;
 
 class Person {
+    string name;
+    char gender;
+    // data de nascimento (pesquisar)
+
+public:
+    Person(string name, char gender, string date);
+
+    const string& get_name() const;
+    char get_gender() const;
+    // get_birth() const;
 
 };
 
